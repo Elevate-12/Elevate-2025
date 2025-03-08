fc6de89d7f6947b85f5daee6fd49594899bdf5ea
Elevate-12 <gavin__2023@163.com>
2025-03-08 16:10:14 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-# Elevate-2025
+# Elevate: model-enhanced LLM driven VUI testing on VPA apps
+
+## Introduction
+The voice user interface~(VUI) has been found to commonly suffer from quality, privacy and security issues.
+By conducting systematic and efficient VUI testing, the VUI's behavior is explored and potential weaknesses are identified. 
+However, previous VUI testing approaches lack global and semantic information, preventing them from testing the VUI efficiently.
+
+To mitigate semantic loss in VUI testing, we propose to integrate Large Language Models(LLMs), which have been successfully applied to several semantic-based software testing domains.
+Since a systematic VUI testing process involves multiple interdependent tasks, a multi-agent LLM framework composed of three specialized LLM agents is introduced.
+To provide the testing system with global guidance, we propose to enhance these agents with a behavior model.
+The behavior model serves as an abstraction of the VUI's behavior, progressively constructed through functionality-level states and context-related input events.
+During testing, task-specific model information is integrated into prompts to provide a structured overview of the VUI's behavior.
+
+Based on these ideas, we implement Elevate, who integrates a multi-agent architecture with model-enhancement technology for effective VUI testing.
+Elevate is implemented on GPT-4-Turbo and DeepSeek-R1, and compared with state-of-the-art tool Vitas and LLM-based chatbot.
+The results demonstrate that Elevate significantly outperforms existing methods in problem detection and state space coverage.
+Finally, Elevate covers 15% higher coverage than state-of-the-art on a large-scale dataset involving 4,000 Alexa skills.
+
+## Files Tree
+
+```text
+├── code
+│   ├── main.py
+│   ├── step1_process_document.py
+│   ├── step2_test_skill.py
+│   ├── step3_detect_problem.py
+│   ├── model
+│   │   └── ...
+│   ├── skill
+│   │   └── ...
+│   └── util
+│       └── ...
+├── config
+│   └── config000.ini
+├── corpus
+│   └── ...
+├── dataset
+│   ├── benchmark_stable.xlsx
+│   └── large_scale.xlsx
+├── README.md
+└── requirement.txt
+```
+
+* code: contains the source code of Elevate
+* config: the configuration file.
+* dataset: Small-Complex-Dataset and Large-Stable-Dataset
+* requirement.txt: requirements of python packages
+
+## Requirement
+
+### packages
+`pip install -r requirement.txt`
+
+`python -m spacy download en_core_web_sm`
+
+### environment
+
+To use the Azure OpenAI GPT-4-Turbo:
+1. Execute: `export OPENAI_API_KEY=<YOUR OPENAI API KEY>`
+2. Change the configuration file **config000.ini** in the config directory, replace apibase and apiversion based on the configuration in your own deployment.
+
+To use DeepSeek-R1:
+1. Execute: `export DEEPSEEK_API_KEY=<YOUR DEEPSEEK API KEY>`
+
+### other
+1. The **chromedriver** that matches your chrome version
+2. An **amazon developer account** that can use the simulator (change the config000.ini in the config directory to add your acount)
+3. an **Azure account** that has access to the openAI API, or a **DeepSeek account** that has access to the DeepSeek API.
+4. We need to login to the amazon developer account to start using the simulator. In most of the cases amazon will send an email to your linked email address for verifications. We use pop to read the emails, so make sure the **110 port** is open.
+
+## How to run Elevate
+
+```
+cd code
+
+python main.py 
+    -i <begin index of skill, default as 1> 
+    -ei <end index of skill, default as 4001> 
+    -c <configuration file name in the config directory, default as config000.ini> 
+    -e <input skills file in the dataset directory, default as large_scale.xlsx> 
+    -l <path to save communication logs and results, default as ../../output/large_scale> 
+    -m <input the LLM to implement Elevate, possible selections are GPT4 and DeepSeek, default as DeepSeek>
+```
---
+++ code/main.py
@@ -0,0 +1,108 @@
+import os
+import re
+import sys
+import argparse
+from model.FSM import FSM
+import util.deal_with_UI as UI
+from util.Spider import Spider
+from util.Deepseek import askDeepseek
+from util.ChatGPT import askChatGPT
+import util.Constant as Constant
+from skill.Skill import Skill
+import step2_test_skill as test
+
+def getArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", help = "input the begin index", dest = "index", type=int, default=1)
+    parser.add_argument("-ei", help = "input the end index", dest = "endindex", type=int, default=4001)
+    parser.add_argument("-c", help = "input the configuration file id", dest = "id", type=str, default="0")
+    parser.add_argument("-e", help = "input the name of an excel file in the dataset directory", dest = "excel_name", type = str, default = "large_scale.xlsx")
+    parser.add_argument("-l", help = "input the path to save logs", dest = "log_path", type = str, default = "../../output/large_scale/")
+    parser.add_argument("-m", help = "input the LLM", dest = "llm", type = str, default = "DeepSeek")
+    args = parser.parse_args()
+    INDEX = args.index
+    END_INDEX = args.endindex
+    CONFIG_ID = args.id
+    EXCEL_PATH = os.path.join(os.path.abspath(r".."), "dataset", args.excel_name)
+    LOG_PATH = args.log_path
+    LLM = args.llm
+    return INDEX, END_INDEX, CONFIG_ID, EXCEL_PATH, LOG_PATH, LLM
+
+def init_dir(LOG_PATH):
+    if not os.path.exists('../cookie'):
+        os.makedirs("../cookie/")
+    if not os.path.exists(LOG_PATH):
+        os.makedirs(LOG_PATH)
+
+def init_constant(config_id):
+    if len(config_id) == 1:
+        Constant.CONFIG_PATH = os.path.join(os.path.abspath(r".."), 'config', 'config00' + config_id + '.ini')
+    elif len(config_id) == 2:
+        Constant.CONFIG_PATH = os.path.join(os.path.abspath(r".."), 'config', 'config0' + config_id + '.ini')
+    else:
+        print("invalid configuration file id")
+        sys.exit(-1)
+    Constant.LOGOUT_URL = "https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26action%3Dsign-out%26path%3D%252Fgp%252Fyourstore%252Fhome%26ref_%3Dnav_AccountFlyout_signout%26signIn%3D1%26useRedirectOnSuccess%3D1"
+    Constant.COOKIE_DIR = os.path.join(os.path.abspath(r".."), "cookie", "console_cookie7_" + config_id + ".pkl")
+    Constant.WEB = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
+    Constant.USER_AGENT = {'User-Agent':Constant.WEB,'Host':'www.amazon.com'}
+    Constant.RE_DIC = {
+        'alert': f'<div class="a-alert-content">(.*?)</div>',
+        'title': f'<meta name="title" content=(.*?)>',
+        'description': '<div data-cy="skill-product-description-see-more" class="sc-kIeTtH dfCbWS">(.*?)</div>',
+        'invocation': f'''<span data-cy="invocation-name-value-detail" font-size="14px" class="sc-eggNIi sc-bsipQr eCEvt hsFSWY">(.*?)</span>''',
+        'permissions': f'<li class="a2s-permissions-list-item">(.*?)</li>',
+        'perm': f'''<span class="a-list-item">
+            
+            
+            
+            
+            (.*?)
+        
+        
+        </span>''',
+        'tit': f'<title>(.*?)</title>'
+    }
+    Constant.TIME_LIMIT = 10 * 60
+    Constant.StopSign = ['Stop', 'stop', 'Exit', 'exit', 'cancel', 'Cancel', 'quit', 'Quit']
+    Constant.SYSTEM_LEVEL_LABEL = "system-level"
+    Constant.HELP_EMBEDDED_LABEL = "help-embedded"
+    Constant.CONTEXT_RELATED_LABEL = "context-related"
+
+if __name__ == '__main__':
+    INDEX, END_INDEX, CONFIG_ID, EXCEL_PATH, LOG_PATH, LLM = getArgs()
+    if not os.path.exists(EXCEL_PATH):
+        print("the excel path does not exist")
+        sys.exit()
+    init_constant(CONFIG_ID)
+    init_dir(LOG_PATH)
+    spider = Spider(Constant.CONFIG_PATH)
+    UI.open_log_page(spider)
+    while True:
+        if INDEX >= END_INDEX:
+            break
+        skill = Skill(EXCEL_PATH, INDEX)
+        if skill.skillName == '<end_of_excel>':
+            break
+        if skill.skillName != '' and len(skill.invocation) > 0:
+            skill_log_dir = os.path.join(LOG_PATH, re.sub(r'(\W+)', '_', skill.type))
+            if not os.path.exists(skill_log_dir):
+                os.makedirs(skill_log_dir)
+            result_path = os.path.join(skill_log_dir, "result")
+            if not os.path.exists(result_path):
+                os.makedirs(result_path)
+            skill_log_path = os.path.join(skill_log_dir, re.sub(r'(\W+)', '_', skill.skillName))
+            if not os.path.exists(skill_log_path):
+                os.makedirs(skill_log_path)
+            else:
+                INDEX = INDEX + 1
+                continue
+            if LLM == "DeepSeek":
+                gpt = askDeepseek(skill.skillName, skill_log_path, True)
+            else:
+                gpt = askChatGPT(skill.skillName, skill_log_path, True, Constant.CONFIG_PATH)
+            fsm = FSM(gpt)
+            test.generateTest(skill_log_path, result_path, spider, skill, gpt, fsm)
+            UI.re_open_with_no_exit(spider)
+        INDEX = INDEX + 1
+    UI.close_spider(spider)
